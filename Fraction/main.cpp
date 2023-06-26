12ccff2305802d61c24363ef992c4f6b3554f1d5
tehiv <tehiv@ASUS-ROG-DSKTP>
2023-06-26 17:33:38 +0300

    Fraction  - solved;

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -1,1 +1,135 @@
 #include <iostream>
+
+class Fraction
+{
+	int integer;  //целое
+	int numerator; //числитель
+	int denominator; //знаминатель
+
+public:
+	int get_integer()const
+	{
+		return integer;
+	}
+	int get_numerator()const
+	{
+		return numerator;
+	}
+	int get_denominator()const
+	{
+		return denominator;
+	}
+
+	void set_integer(int integer)
+	{
+		this->integer = integer;
+	}
+	void set_numerator(int numenator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denominator(int denominator)
+	{
+		if (denominator == 0) denominator = 1;
+		this->denominator = denominator;
+	}
+
+	//		 constructors:
+
+	Fraction() // no param
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		std::cout << "Default Construct: " << this << std::endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		std::cout << "Single-arg constructor: " << std::endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		this->numerator = numerator;
+		set_denominator(denominator);
+		std::cout << "Constructor:\t" << std::endl;
+	}
+
+	Fraction(int integer, int numerator, int denominator)
+	{
+		this->integer = integer;
+		this->numerator = numerator;
+		set_denominator(denominator);
+		std::cout << "Constructor:\t" << this << std::endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		std::cout << "Copy constructor: "<< this << std::endl;
+	}
+	~Fraction()
+	{
+		std::cout << "Destructor:\t" << this << std::endl;
+	}
+
+	//	       operators:
+	Fraction&operator=(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		std::cout << "CopyAssignment:" << this << std::endl;
+		return *this;
+	}
+
+	//          methods:
+
+	void print()const
+	{
+		if (integer)std::cout << integer;  
+		if (numerator)
+		{
+			if (integer)std::cout << "(";
+			std::cout << numerator << "/" << denominator;
+			if (integer)std::cout << ")";
+		}
+		else if (integer == 0) std::cout << 0;
+		std::cout << std::endl;
+	}
+};
+
+//#define CONSTRUCTORS_CHECK;
+
+void main()
+{
+#ifdef CONSTRUCTORS_CHECK
+	setlocale(LC_ALL, "");
+
+	Fraction A; //default constructor
+	A.print();
+
+	Fraction B = 5;
+	B.print();
+
+	Fraction C(1, 2);
+	C.print();
+
+	Fraction D(2, 3, 4);
+	D.print();
+
+	Fraction  E = C; //copy constructor
+	E.print();
+
+	Fraction F;
+	F = D;
+	F.print();
+	#endif// Constructors check
+
+
+
+}
