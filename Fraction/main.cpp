08e9e08f664124c2e3af834b73457d7a447ebfce
tehiv <tehiv@ASUS-ROG-DSKTP>
2023-06-30 16:59:10 +0300

    Reducing done;

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
 
 class Fraction;
-Fraction operator*(Fraction left, Fraction right);
+Fraction operator * (Fraction left, Fraction right);
 Fraction operator / (const Fraction& left, const Fraction& right);
 Fraction operator + (const Fraction& left, const Fraction& right);
 Fraction operator - (const Fraction& left, const Fraction& right);
-
+bool operator == (Fraction left, Fraction right);
+bool operator != (const Fraction left, const Fraction right);
+bool operator > (Fraction left, Fraction right);
+bool operator < (Fraction left, Fraction right);
 
 class Fraction
 {
@@ -47,39 +50,39 @@ public:
 		this->integer = 0;
 		this->numerator = 0;
 		this->denominator = 1;
-		std::cout << "Default Construct: " << this << std::endl;
+		std::cout << " + Default Constructor: " << this << std::endl;
 	}
 	Fraction(int integer)
 	{
 		this->integer = integer;
 		this->numerator = 0;
 		this->denominator = 1;
-		std::cout << "Single-arg constructor: "<< this << std::endl;
+		std::cout << " + Single-arg constructor: "<< this << std::endl;
 	}
 	Fraction(int numerator, int denominator)
 	{
 		this->integer = 0;
 		this->numerator = numerator;
 		set_denominator(denominator);
-		std::cout << "Constructor:\t" << this << std::endl;
+		std::cout << " + Constructor:\t  " << this << std::endl;
 	}
 	Fraction(int integer, int numerator, int denominator)
 	{
 		this->integer = integer;
 		this->numerator = numerator;
 		set_denominator(denominator);
-		std::cout << "Constructor:\t" << this << std::endl;
+		std::cout << " + Constructor:\t  " << this << std::endl;
 	}
 	Fraction(const Fraction& other)
 	{
 		this->integer = other.integer;
 		this->numerator = other.numerator;
 		this->denominator = other.denominator;
-		std::cout << "Copy constructor: "<< this << std::endl;
+		std::cout << " + Copy constructor: "<< this << std::endl;
 	}
 	~Fraction()
 	{
-		std::cout << "Destructor:\t" << this << std::endl;
+		std::cout << " ~ Destructor:\t  " << this << std::endl;
 	}
 
 //========= operators ===========
@@ -178,6 +181,28 @@ public:
 		std::swap(inverted.numerator, inverted.denominator);
 		return inverted;
 	}
+	Fraction& reduce()
+	{/*
+		int more, less, rest;
+		if (numerator > denominator) more = numerator, less = denominator;
+		else less = numerator, more = denominator;*/
+		to_proper();
+		int less = numerator;
+		int more = denominator;
+		int rest;
+		do
+		{
+			rest = more % less;
+			more = less;
+			less = rest;
+		}
+		while(rest);
+
+		int GCD = more; //GCD - Greatest Common Divisor;
+		numerator /= GCD;
+		denominator /= GCD;
+		return *this;
+	}
 };
 
 Fraction operator * (Fraction left, Fraction right)
@@ -222,8 +247,46 @@ Fraction operator - (const Fraction& left, const Fraction& right)
 		left.get_denominator() - right.get_denominator()
 	);
 }
+bool operator == (Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	/*if (left.get_numerator() * right.get_denominator() == right.get_numerator() * left.get_denominator())
+		return true;
+	else
+		return false;*/
+	return left.get_numerator() * right.get_denominator() == right.get_numerator() * left.get_denominator();
+}
+bool operator != (const Fraction left, const Fraction right)
+{
+	return !(left == right);
+}
+bool operator > (Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return left.get_numerator() * right.get_denominator() > right.get_numerator() * right.get_denominator();
+}
+bool operator < (Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return left.get_numerator() * right.get_denominator() <  right.get_numerator() * right.get_denominator();
+}
+bool operator >= (const Fraction& left, const Fraction& right)
+{
+	//return left > right || left == right;
+	return !(left < right); //better performance, 'cause operator "==" is not involved;
+}
+bool operator <= (const Fraction& left, const Fraction& right)
+{
+	return !(left > right); 
+}
 
 //#define CONSTRUCTORS_CHECK
+//#define ARITHMETICAL_OPERATORS_CHECK
+//#define COMPARISON_OPERATORS_CHECK
+//#define REDUCING_CHECK
 
 void main()
 {
@@ -248,8 +311,8 @@ void main()
 	Fraction F; //assingment operator
 	F = D;
 	F.print();
-	#endif// Constructors check
-
+	#endif// Constructors check;
+#ifdef ARITHMETICAL_OPERATORS_CHECK
 	Fraction A(2, 3, 4);
 	A.print();
 
@@ -264,4 +327,21 @@ void main()
 	A *= B;
 	A.print();
 
+#endif //Arithmetical operators check;
+#ifdef COMPARISON_OPERATORS_CHECK
+	Fraction A(1, 2);
+	Fraction B(5, 10);
+	std::cout << (A <= B) << std::endl;
+#endif //Comparison operations check;
+#ifdef REDUCING_CHECK
+	Fraction A(5, 10);
+	A.print();
+	A.reduce();
+	A.print();
+#endif //reducing check;
+
+
 }
+
+// The "," operator allows to write several expressions in the place where one expression is expected;
+// All expressions will be sequentially executed from left to right, and the operator "," will return only the value of the [!]last expression;
