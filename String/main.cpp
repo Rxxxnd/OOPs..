b8eb921aecee05e7e17b8e31b559517c5e41b4bb
tehiv <tehiv@ASUS-ROG-DSKTP>
2023-07-05 17:33:27 +0300

    String in progress;

--- String/main.cpp
+++ String/main.cpp
@@ -2,17 +2,34 @@
 
 class String
 {
-	int size;
-	char str;
+	int size;  // size of string in bytes;
+	char* str; // adress of string in dynamic memory
 
 public:
-	int get_size()const
+	String(int size = 80)
 	{
-		return size;
+		this->size = size;
+		this->str = new char[size] {};
+		std::cout << " + Default Constructor: " << this << std::endl;
 	}
+	~String()
+	{
+		delete[] str;
+		std::cout << " - Destructor: " << this << std::endl;
+	}
+
+	// ============= methods ==============
+
+	void print()const
+	{
+		std::cout << "Size: \t" << size << std::endl;
+		std::cout << "Str: \t" << str << std::endl;
+	}
+
 };
 
 void main()
 {
-
+	String str;
+	str.print();
 }
