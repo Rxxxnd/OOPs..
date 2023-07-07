7ebbc9c599f48f11256e8e2d659d6c478d09d023
tehiv <tehiv@ASUS-ROG-DSKTP>
2023-07-07 12:08:38 +0300

    code rewriting completed;

--- String/main.cpp
+++ String/main.cpp
@@ -2,30 +2,27 @@
 
 class String
 {
-	int size;  // size of string in bytes;
-	char* str; // adress of string in dynamic memory
-
+	int size;
+	char* str;
 public:
 	String(int size = 80)
 	{
 		this->size = size;
 		this->str = new char[size] {};
-		std::cout << " + Default Constructor: " << this << std::endl;
+		std::cout << " + Default constructor: " << this << std::endl;
 	}
 	~String()
 	{
 		delete[] str;
 		std::cout << " - Destructor: " << this << std::endl;
 	}
+	// ========== metords ==========
 
-	// ============= methods ==============
-
-	void print()const
+	void print() const
 	{
-		std::cout << "Size: \t" << size << std::endl;
-		std::cout << "Str: \t" << str << std::endl;
+		std::cout << " Size: \t" << size << std::endl;
+		std::cout << "  Str: \t" << str << std::endl;
 	}
-
 };
 
 void main()
