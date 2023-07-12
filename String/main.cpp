c9882de8754781b3a46b26261c83005f347edcff
tehiv <tehiv@ASUS-ROG-DSKTP>
2023-07-12 16:53:36 +0300

    Header initialization DONE;

--- String/main.cpp
+++ String/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#define delimiter "\n----------------------------------------------------------------------------\n"
+#define delimiter "\n---------------------------------\n"
 
 class String
 {
@@ -20,25 +20,27 @@ public:
 	{
 		return str;
 	}
+//  ========== Constructors ==========
 
-	explicit String(int size = 80)
+	explicit String(int size = 80) :size(size), str(new char[size] {}) // [!!!!!]
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;  // ==  :size(size); [!!!!!]
+		//this->str = new char[size] {}; //  == str(new char[size] {}; [!!!!!]
 		std::cout << " + Default constructor: " << this << std::endl;
 	}
-	String(const char* str)
+	String(const char* str) :size(strlen(str)+1), str(new char[size] {})
 	{
-		this->size = strlen(str) + 1; // +1 bc of adding 0;
-		this->str = new char[size] {};
+		int a(2);
+		//this->size = strlen(str) + 1; // +1 bc of adding 0;   == :size(strlen(str)+1)
+		//this->str = new char[size] {};   // == str(new char[size] {})
 		for (int i = 0; str[i]; i++) this->str[i] = str[i];
-		std::cout << " + Constructor:\t" << this << std::endl;
+		std::cout << " + Constructor:\t\t" << this << std::endl;
 	}
-	String(const String& other)
+	String(const String& other) :size(other.size), str(new char[size] {})
 	{
 		//Deep copy
-		this->size = other.size;
-		this->str = new char[size] {};
+		//this->size = other.size;
+		//this->str = new char[size] {};
 
 		for (int i = 0; i < size; i++) this->str[i] = other.str[i];
 		std::cout << " + Copy constructor:\t" << this << std::endl;
@@ -67,11 +69,11 @@ public:
 		return *this;
 	}
 
-	String(String&& other) //&& - r-value reference;
+	String(String&& other):size(other.size), str(other.str) //&& - r-value reference;
 	{
 		//Shallow copy;
-		this->size = other.size;
-		this->str = other.str;
+		//this->size = other.size;   // == :size(other.size)
+		//this->str = other.str;     // == str(other.str)
 		std::cout << " + MoveConstructor:\t" << this << std::endl;
 		other.size = 0;
 		other.str = 0;
@@ -80,7 +82,7 @@ public:
 	~String()
 	{
 		delete[] str;
-		std::cout << " - Destructor:\t\t" << this << std::endl;
+		std::cout << " ~ Destructor:\t\t" << this << std::endl;
 	}
 	// ========== Operators ==========
 
@@ -114,8 +116,11 @@ public:
 		return os << obj.get_str();
 	}
 
+#define BASE_CHECK
+//#define CALLING_CONSTRUCTORS
 void main()
 {
+#ifdef BASE_CHECK
 	String str (5);
 	str.print();
 
@@ -125,7 +130,7 @@ void main()
 
 	std::cout << delimiter << std::endl;
 
-	String str2 = "World";
+	String str2 = " World";
 	std::cout << str2 << std::endl;
 
 	std::cout << delimiter << std::endl;
@@ -137,6 +142,33 @@ void main()
 	String str4;
 	str4 = str1 + str2;
 	std::cout << str4 << std::endl;
+	
+	String str5 = str4;
+	std::cout << str5 << std::endl;
+
+#endif
+
+#ifdef CALLING_CONSTRUCTORS
+	String str1;    //Default constructor;
+	str1.print();
+
+	String str2(5);    //explicit constructor
+		str2.print();
+
+	String str3 = "Hello";  //single argument
+		str3.print();
+
+	String str4();  // functon "str4()" - no parameters and returns "String";
+
+	//For explicit default constructor:
+
+	String str5{};  //explicit default constructor;
+	str5.print();
+
+	std::cout << String() << std::endl;
+	String().print();
+#endif
+
 
 }
 
